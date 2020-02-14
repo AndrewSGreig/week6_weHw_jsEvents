b70016c0b714ac7160e3a997659b0975abf249d1
User <user@Users-MacBook-Pro.local>
2020-02-14 15:44:06 +0000

    basic functionality working

---
+++ js/app.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('JavaScript loaded');
+
+
+  const handleFormSubmit = function(event){
+    event.preventDefault();
+    console.log('in the function');
+    const holidayList = document.querySelector('#holiday-list');
+    const newLi = document.createElement('li');
+    newLi.textContent = `Your destination: ${event.target.location.value}, ${event.target.country.value}, ${event.target.continent.value}`;
+
+    holidayList.appendChild(newLi);
+
+    form.reset();
+  }
+  // add an event listener to the form
+  // 1) get the form
+  const form = document.querySelector('#new-item-form');
+
+  // 2) attach the event listener for the submit event from the save button
+  form.addEventListener('submit', handleFormSubmit);
+
+
+})
