e0720f65174ef5a0d1bf368b4008a77d809c0cb6
User <user@users-mbp.home>
2020-02-16 10:29:02 +0000

    changed continents to be radio buttons

--- js/app.js
+++ js/app.js
@@ -1,12 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript loaded');
 
+  // const continent = function(event){
+  //   console.log('in continent function');
+  //   const europe = document.createElement("INPUT");
+  //   europe.setAttribute("type", "radio");
+  //   europe.setAttribute("id", "europe");
+  //   document.body.appendChild(europe);
+  //
+  //   const asia = document.createElement("INPUT");
+  //   asia.setAttribute("type", "radio");
+  //   document.body.appendChild(asia);
+  // }
+
 
   const handleFormSubmit = function(event){
     event.preventDefault();
+    debugger;
     console.log('in the function');
     const holidayList = document.querySelector('#holiday-list');
     const newLi = document.createElement('li');
+    // if (event.target.continent.europe
     newLi.textContent = `Your destination: ${event.target.location.value}, ${event.target.country.value}, ${event.target.continent.value}`;
 
     holidayList.appendChild(newLi);
@@ -35,5 +49,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const deleteAllButton = document.querySelector('#delete-all');
   deleteAllButton.addEventListener('click', handleDeleteAllClick);
+
+
+
 
 })
